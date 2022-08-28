76b43d9cde5c90e00ed56c087d28e076c813925c
Harashima <harashima318@gmail.com>
2022-08-28 18:26:06 +0900

    [update]アニメーションの遷移を設定

--- Assets/Scripts/NovaController.cs
+++ Assets/Scripts/NovaController.cs
@@ -2,20 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody), typeof(Animator))]
 public class NovaController : MonoBehaviour
 {
+    //必要なコンポーネント
     Rigidbody _rigidBody = null;
+    Animator _anim = null;
+
+    //パラメータ
     [Header("プレイヤーの移動スピード"), SerializeField]
     float _speed = 5f;
+
+    float _hor = 0f;
+    float _ver = 0f;
 
     private void Awake()
     {
         _rigidBody = GetComponent<Rigidbody>();
+        _anim = GetComponent<Animator>();
     }
 
-    float _hor = 0f;
-    float _ver = 0f;
     private void FixedUpdate()
     {
         Move();
@@ -24,6 +30,8 @@ public class NovaController : MonoBehaviour
     {
         _hor = Input.GetAxisRaw("Horizontal");
         _ver = Input.GetAxisRaw("Vertical");
+        _anim.SetBool("IsWalk", Mathf.Abs(_ver) > 0.1f || Mathf.Abs(_hor) > 0.1f);
+
         Vector3 dir = Vector3.forward * _ver + Vector3.right * _hor;
         if (dir == Vector3.zero)
         {
@@ -35,6 +43,7 @@ public class NovaController : MonoBehaviour
             dir = Camera.main.transform.TransformDirection(dir);    // メインカメラを基準に入力方向のベクトルを変換する // y 軸方向はゼロにして水平方向のベクトルにする // そのベクトルの方向にオブジェクトを向ける
 
             dir.y = 0;
+            this.transform.forward = dir;
             // 前方に移動する。ジャンプした時の y 軸方向の速度は保持する
             _rigidBody.velocity = dir * _speed;
         }
