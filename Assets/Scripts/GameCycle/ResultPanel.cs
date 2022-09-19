cfde82b71dd5d948c9567b58e7e5d07c5c9410a0
HarashimaYoshikazu <harashima318@gmail.com>
2022-09-19 18:54:13 +0900

    [add]リザルト関係のクラスとプレハブを追加。

---
+++ Assets/Scripts/GameCycle/ResultPanel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ResultPanel : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _panel;
+
+    [SerializeField]
+    Text _text;
+
+    [SerializeField]
+    Button _restartButton;
+
+    private void Awake()
+    {
+        _restartButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        });
+    }
+
+    public void Init(string text)
+    {
+        _panel.SetActive(true);
+        _text.text = text;
+    }
+}
